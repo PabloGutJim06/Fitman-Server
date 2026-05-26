7a423fe589f91f4bcf607c187f24542af8a2e909
PabloGutJim06 <pablogutjim06@gmail.com>
2026-05-26 10:52:02 +0200

    Version 4.3
    Crear Rutina por defecto

--- services/usuario.service.js
+++ services/usuario.service.js
@@ -8,6 +8,67 @@ import {
     actualizarUsuario,
 } from "../repositories/usuario.repository.js"; 
 import { ConflictError } from "../utils/errors.js";
+import {
+    crearRutina,
+} from "../repositories/rutina.repository.js";
+
+
+const _crearRutinaEjemplo = async (usuarioId) => {
+    const rutina = {
+        usuario_id: usuarioId,
+        rutina_nombre: "Mi primera rutina",
+        ejercicios: [
+            {
+                nombre: "Press Banca Plano",
+                descripcion: "",
+                series: 4,
+                repeticiones: 10,
+                duracion: 0,
+                descanso_segundos: 0,
+            },
+            {
+                nombre: "Descanso",
+                duracion: 60,
+                series: 0,
+                repeticiones: 0,
+                descanso_segundos: 0,
+            },
+            {
+                nombre: "Sentadilla con Barra",
+                descripcion: "",
+                series: 4,
+                repeticiones: 8,
+                duracion: 0,
+                descanso_segundos: 0,
+            },
+            {
+                nombre: "Descanso",
+                duracion: 90,
+                series: 0,
+                repeticiones: 0,
+                descanso_segundos: 0,
+            },
+            {
+                nombre: "Dominadas",
+                descripcion: "",
+                series: 3,
+                repeticiones: 8,
+                duracion: 0,
+                descanso_segundos: 0,
+            },
+        ],
+        calorias: 0,
+        duracion_total: 150, // 60 + 90 segundos de descanso
+        colaboradores: [],
+        colaboradores_ids: [],
+    };
+
+    try {
+        await crearRutina(rutina);
+    } catch (e) {
+        console.error("Error al crear rutina de ejemplo:", e.message);
+    }
+};
 
 export const createUsuario = async (datosUsuario) => {
     const usuarioExistente = await buscarPorEmail(datosUsuario.email);
@@ -16,9 +77,10 @@ export const createUsuario = async (datosUsuario) => {
     }
 
     const hashedPassword = await bcrypt.hash(datosUsuario.password, 10);
-
     const usuarioConHash = { ...datosUsuario, password: hashedPassword };
     const id = await crearUsuario(usuarioConHash);
+
+    await _crearRutinaEjemplo(id);
 
     const { password, ...usuarioSinPassword } = usuarioConHash;
     return { id, ...usuarioSinPassword };
